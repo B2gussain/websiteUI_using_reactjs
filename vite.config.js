8cc6c0c89768ff417a2ce685a1d50d4a5d7594ee
B2gussain <bittuggussain@gmail.com>
2024-10-21 12:38:49 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: "0.0.0.0", // This allows external access
+    port: 5173, // Default to port 3000 if not defined
+  },
 })
